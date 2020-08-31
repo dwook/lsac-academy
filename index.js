af07a105ff3f6006f156a422f1318b9cead6ddff
dwook <igerapex@gmail.com>
2020-08-31 12:21:46 +0900

    Fix mouse event

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 var cursor = $(".cursor"),
   follower = $(".cursor-follower");
 
-var posX = 0,
-  posY = 0,
-  mouseX = 0,
-  mouseY = 0;
-
-TweenMax.to({}, 0.016, {
-  repeat: -1,
-  onRepeat: function () {
-    posX += (mouseX - posX) / 9;
-    posY += (mouseY - posY) / 9;
-
-    TweenMax.set(follower, {
-      css: {
-        left: posX - 20,
-        top: posY - 20,
-      },
-    });
-
-    TweenMax.set(cursor, {
-      css: {
-        left: mouseX,
-        top: mouseY,
-      },
-    });
-  },
-});
-
 (function () {
   $(function () {
+    var posX = 0;
+    var posY = 0;
+    var mouseX = 0;
+    var mouseY = 0;
+
+    TweenMax.to({}, 0.016, {
+      repeat: -1,
+      onRepeat: function () {
+        posX += (mouseX - posX) / 9;
+        posY += (mouseY - posY) / 9;
+
+        TweenMax.set(follower, {
+          css: {
+            left: posX - 20,
+            top: posY - 20,
+          },
+        });
+
+        TweenMax.set(cursor, {
+          css: {
+            left: mouseX,
+            top: mouseY,
+          },
+        });
+      },
+    });
+
+    $(document).on("mousemove", function (e) {
+      mouseX = e.pageX;
+      mouseY = e.pageY;
+    });
+
     var elements = $(".text, .img").toArray();
     $(window).scroll(function () {
       elements.forEach(function (item) {
